e17ec0f99dccd01b8abda93b27d83aa76f425371
npinedajr <74744615+npinedajr@users.noreply.github.com>
2020-11-26 17:50:37 -0700

    Add files via upload

---
+++ IRReciever.ino
@@ -0,0 +1,42 @@
+// Include IR Remote Library by Ken Shirriff
+#include <IRremote.h>
+ 
+const int RECV_PIN = 10; //The infrared reciever will be connected to pin 10 of the Arduino.
+const int RELAY = 2; //The relay module will be connected to pin 2 of the Arduino
+ 
+// Define irrecev and results Objects
+IRrecv irrecv(RECV_PIN); //Define a IRRecv object, which will be utilizing the infrared reciever.
+decode_results results; //Define a decode_results object, which tracks what signal was recieved.
+bool longPress;
+ 
+void setup(){
+  // Serial Monitor @ 9600 baud
+  pinMode(RELAY, OUTPUT); //Set the relay module as an output. 
+  digitalWrite(RELAY,LOW);
+  irrecv.enableIRIn(); //Allow the infrared reciever to recieve signals. 
+  longPress = false;
+}
+
+/* The following activates different code blocks, depending on the signal that the infrared reciever
+ * recieves, which changes the behaviour of the relay module connected. If the infrared reciver no longer
+ * recieves a signal, and the signal that was just recieved implies that a button was pressed down for a long
+ * time, it will return the relay module to it's initial state. 
+ */
+void loop(){
+  if(irrecv.decode(&results)){
+    if(results.value == 0xFEA857){
+      digitalWrite(RELAY, LOW);
+      delay(100);
+      digitalWrite(RELAY, HIGH);
+      delay(4000);
+      irrecv.resume();
+  } else if(results.value == 0xFECA34){
+      digitalWrite(RELAY,HIGH);
+      longPress = true;
+  } 
+  else if(longPress = true){
+    digitalWrite(RELAY, LOW);
+    longPress = false;
+   }
+ }
+}
---
+++ IRRemote.ino
@@ -0,0 +1,41 @@
+// Include IR Remote Library by Ken Shirriff
+#include <IRremote.h>
+
+IRsend irsend;
+
+unsigned long pressedMillis; //Will track at what millisecond the button connected to pin 8 has been pressed. 
+const unsigned holdPeriod = 1500; //The button must be held for 1.5 seconds to be considered a long press. Can be adjusted based on needs.
+bool longPress; 
+int currentState;
+int previousState; //previousState tracks if currentState is HIGH or LOW in the previous loop.
+
+void setup()
+{
+  pinMode(8, INPUT); //Pin 8 of an Arduino will have a button connected, which will serve as input.
+  longPress = false; 
+  currentState = LOW;
+  previousState = LOW;
+}
+
+/* The following activates different blocks of code depending on the state of the button connected to 
+ * pin 8. If the button is pressed down for at least 1.5 seconds, then the infrared led connected 
+ * to the Arduino will send a signal that a infrared reciever will decode. If the button is pressed for 
+ * less than 1.5 seconds, then the infrared led will send a different signal for the infrared reciever to decode. 
+ */
+void loop()
+{
+  currentState = digitalRead(8); // When the button connected to pin 8 in the Arduino is pressed, currentState will be HIGH. Otherwise, it will be LOW. 
+  if(currentState == HIGH && previousState == LOW){
+    pressedMillis = millis();
+  }
+  if(currentState == HIGH && (millis()-pressedMillis)>holdPeriod){
+    irsend.sendNEC(0xFECA34,32);
+    longPress = true;
+  }
+  if(currentState == LOW && previousState == HIGH && longPress == false){
+    irsend.sendNEC(0xFEA857,32);
+  } else {
+    longPress = false;
+  }
+  previousState = currentState; 
+}
